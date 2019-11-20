64b94f311770ad0cfaa2f3a4e7b6110e4a2cdb28
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-11-20 12:09:53 +0300

    test out graphql server

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -1,1 +1,29 @@
-console.log('Hello From Graphql...');
+import { GraphQLServer } from "graphql-yoga";
+
+/**
+ * Schema - (Type definitions)
+ */
+
+const typeDefs = `
+  type Query {
+      hello: String!
+  }
+ `;
+
+/**
+ * Resolvers
+ */
+const resolvers = {
+  Query: {
+    hello() {
+      return "Hello, GraphQL yoga!";
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+
+server.start(() => console.log("Server Up..."));
