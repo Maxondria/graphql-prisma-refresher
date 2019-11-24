cc7dd7d25e3042fb696b10cb824f538c0ab5ae34
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-11-24 21:16:52 +0300

    fetch data -users and posts from prisma

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -7,21 +7,23 @@ export default {
     return db.users.find(user => user.id === Number(args.id));
   },
 
-  posts(_parent, args, { db }, _info) {
-    if (args.query) {
-      return db.posts.filter(post =>
-        post.title.toLowerCase().includes(args.query.toLowerCase())
-      );
-    } else return db.posts;
+  async posts(_parent, args, { prisma }, info) {
+    return await prisma.query.posts(null, info);
+    // if (args.query) {
+    //   return db.posts.filter(post =>
+    //     post.title.toLowerCase().includes(args.query.toLowerCase())
+    //   );
+    // } else return db.posts;
   },
 
-  users(_parent, args, { db }, _info) {
-    if (args.query) {
-      return db.users.filter(user =>
-        user.name.toLowerCase().includes(args.query.toLowerCase())
-      );
-    }
-    return db.users;
+  async users(_parent, args, { prisma }, info) {
+    return await prisma.query.users(null, info);
+    // if (args.query) {
+    //   return db.users.filter(user =>
+    //     user.name.toLowerCase().includes(args.query.toLowerCase())
+    //   );
+    // }
+    // return db.users;
   },
 
   comments(_parent, _args, { db }, _info) {
